d4530a1473d04bdab3635963ac0054087dba4aed
zhilong <wisgonchen@gmail.com>
2022-12-18 19:52:40 +0900

    ant design ready

--- electron_ui/src/renderer/App.tsx
+++ electron_ui/src/renderer/App.tsx
@@ -1,50 +1,66 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+// import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+// import icon from '../../assets/icon.svg';
+// import './App.css';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+// const Hello = () => {
+//   return (
+//     <div>
+//       <div className="Hello">
+//         <img width="200" alt="icon" src={icon} />
+//       </div>
+//       <h1>electron-react-boilerplate</h1>
+//       <div className="Hello">
+//         <a
+//           href="https://electron-react-boilerplate.js.org/"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="books">
+//               📚
+//             </span>
+//             Read our docs
+//           </button>
+//         </a>
+//         <a
+//           href="https://github.com/sponsors/electron-react-boilerplate"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="folded hands">
+//               🙏
+//             </span>
+//             Donate
+//           </button>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Hello />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+import { Button, DatePicker, Space, version } from 'antd';
+import 'antd/dist/antd.css';
+import './App.css';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <h1>antd version: {version}</h1>
+      <Space>
+        <DatePicker />
+        <Button type="primary">Primary Button</Button>
+      </Space>
+    </div>
   );
 }
